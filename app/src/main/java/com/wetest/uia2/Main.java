740f8f495ad8b73135f1697a3cf89c1bdbe5e162
zhongkaizhu <zhongkaizhu@tencent.com>
2024-07-04 14:46:26 +0800

    app/wetest: fix Bad Descriptor and debug in AndroidStudio not work

--- app/src/main/java/com/wetest/uia2/Main.java
+++ app/src/main/java/com/wetest/uia2/Main.java
@@ -1,7 +1,11 @@
 package com.wetest.uia2;
 
+import android.app.Instrumentation;
+import android.os.Looper;
 import android.os.SystemClock;
+import android.view.accessibility.AccessibilityNodeInfo;
 
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 
@@ -12,12 +16,17 @@ import com.googlecode.jsonrpc4j.JsonRpcServer;
 import com.wetest.uia2.stub.AutomatorHttpServer;
 import com.wetest.uia2.stub.AutomatorService;
 import com.wetest.uia2.stub.AutomatorServiceImpl;
+import com.wetest.uia2.stub.Log;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import proxy.Bridge;
 import uiautomator.InstrumentShellWrapper;
 
 
@@ -40,9 +49,11 @@ public class Main {
         Ln.i("[UiAutomator2Server] Starting Server");
         int PORT = 9008;
 
+        // make sure Looper.prepareMainLooper() is called
         InstrumentShellWrapper.getInstance().getContext();
         UiDevice device = UiDevice.getInstance(InstrumentShellWrapper.getInstance());
         device.wakeUp();
+
 
         JsonRpcServer jrs = new JsonRpcServer(new ObjectMapper(), new AutomatorServiceImpl(), AutomatorService.class);
         jrs.setShouldLogInvocationErrors(true);
@@ -63,5 +74,27 @@ public class Main {
         AutomatorHttpServer server = new AutomatorHttpServer(PORT);
         server.route("/jsonrpc/0", jrs);
         server.start();
+
+//        Looper.loop();
+        Log.i("server started");
+        while (server.isAlive()) {
+            if (!checkAccessibilityQuery()) {
+                Log.e("uiAutomation.getRootInActiveWindow() always return null, okhttpd server quit");
+                return;
+            }
+            Thread.sleep(500);
+        }
+    }
+
+    private static boolean checkAccessibilityQuery() throws InterruptedException {
+        // check if app_process still alive
+        for (int i = 3; i > 0; i--) {
+            AccessibilityNodeInfo nodeInfo = UiDevice.getInstance(null).getUiAutomation().getRootInActiveWindow();
+            if (nodeInfo != null) {
+                return true;
+            }
+            if (i > 1) Thread.sleep(1000);
+        }
+        return false;
     }
 }
